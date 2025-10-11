0a3fa1a97575df5f35f69d93ad88f17a2f878608
nameispuneeth <puneethkumarkaramala@gmail.com>
2025-10-11 15:18:08 +0530

    DB Cahnges

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,12 +4,8 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   password: String,
-  // Store Issue IDs in arrays
-  issues: {
-    pending: [{ type: mongoose.Schema.Types.ObjectId, ref: "Issue" }],
-    inprogress: [{ type: mongoose.Schema.Types.ObjectId, ref: "Issue" }],
-    resolved: [{ type: mongoose.Schema.Types.ObjectId, ref: "Issue" }],
-  }
+  issues: [{ type: mongoose.Schema.Types.ObjectId, ref: "Issue" }]
 });
+
 
 module.exports = mongoose.model("User", userSchema);
